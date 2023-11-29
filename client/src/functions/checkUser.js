9c03a1a2521dfe1ddfc44a4bddfe5b41b74f0ed1
mohan-246 <mohan5497306@gmail.com>
2023-11-29 12:37:51 +0530

    changed site origins

--- client/src/functions/checkUser.js
+++ client/src/functions/checkUser.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function checkUser(userId) {
     try {
-      const response = await axios.get(`http://localhost:3001/api/user/${userId}`);
+      const response = await axios.get(`https://chat-backend-itw9.onrender.com/api/user/${userId}`);
       return response.data;
     } catch (error) {
       
--- client/src/functions/getRooms.js
+++ client/src/functions/getRooms.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function getRooms(userId) {
     try {
-      const response = await axios.get(`http://localhost:3001/api/rooms/${userId}`);
+      const response = await axios.get(`https://chat-backend-itw9.onrender.com/api/rooms/${userId}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching rooms:', error);
